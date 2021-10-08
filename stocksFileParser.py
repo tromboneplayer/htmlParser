0962d6aabcc02c4343ec7b45e733e833ad4c5c77
tromboneplayer <allenearnest@gmail.com>
2021-10-07 23:29:47 -0400

    changes for csv file parsing

--- stocksFileParser.py
+++ stocksFileParser.py
@@ -2,12 +2,38 @@ from datetime import datetime, date
 import json
 from bs4 import BeautifulSoup
 import os
+import csv
 
 
-#sometimes the symbolList needs to be corrected because two (or more) symbols are 
-#combined into the same list entry.  so split each one based on a newline character
-#and then add it back to a newly created list
+def parseCsvFile(path, fileName):
+    '''parse a csv file for the AARK (Cathie Wood) holdings fund'''
+    fileNameParsed = fileName.split(".")[0].split("_")
+    fileSource = fileNameParsed[0]  #ARKK
+    fileDate = fileNameParsed[2]
+    
+    symbolList = list()
+    
+    with open(path+fileName, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in spamreader:
+            stock = row[3]
+            if stock:
+                symbolList.append(stock)
+        del(symbolList[0])
+        symbolList.sort()
+
+    #specify the output file location
+    outputFilename = "./Output/stocks_" + fileSource + "_" + fileDate + ".txt"
+
+    with open(outputFilename, "w") as txt_file:
+        for symbol in symbolList:
+            txt_file.write(symbol + "\n")
+
+
 def fix_list(symbolList):
+    '''sometimes the symbolList needs to be corrected because two (or more) symbols 
+    are combined into the same list entry.  so split each one based on a newline character 
+    and then add it back to a newly created list'''
     new_symbols_set = set()
     for symbol in symbolList:
         symbols = symbol.split("\n")
@@ -17,7 +43,7 @@ def fix_list(symbolList):
     return list(new_symbols_set)
 
 
-def parseFile(path, fileName):
+def parseHtmlFile(path, fileName):
     
     fileNameParsed = fileName.split(".")[0].split("_")
     tmfFileSource = fileNameParsed[1]  #SA = Stock Advisor; RB = Rule Breakers; BS = Back Stage
@@ -53,15 +79,20 @@ def parseFile(path, fileName):
 
 def main():
     
-    print("htmlParser started")
+    print("stocksFileParser started")
     path = "./Input/"  #the folder containing HTML files to parse for The Motley Fool webpages
     files = os.listdir(path)
     
     for fileName in files:
         print("Processing {f}...".format(f=fileName))
-        parseFile(path, fileName)
-        
-    print("htmlParser ended")
+        fileExt = fileName.split(".")[1].lower()
+        if fileExt == "htm":
+            #parseHtmlFile(path, fileName)
+            pass
+        if fileExt == "csv":
+            parseCsvFile(path, fileName)
+
+    print("stocksFileParser ended")
 
 
 if __name__ == "__main__":
